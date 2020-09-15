67c26561e4adc60f60e5233d9d703b2361b6bef3
Judite Denise <judigunkel@gmail.com>
2020-09-15 15:58:29 -0300

    ex009

---
+++ Mundo 1/ex009.py
@@ -0,0 +1,16 @@
+"""
+Faça um programa que leia um número e mostre na tela a sua tabuada
+"""
+num = int(input('Digite um número para ver a sua tabuada: '))
+print('\033[33m=*=\033[m' *5)
+print(f'1  * {num} = \033[34m{num * 1}\033[m')
+print(f'2  * {num} = \033[34m{num * 2}\033[m')
+print(f'3  * {num} = \033[34m{num * 3}\033[m')
+print(f'4  * {num} = \033[34m{num * 4}\033[m')
+print(f'5  * {num} = \033[34m{num * 5}\033[m')
+print(f'6  * {num} = \033[34m{num * 6}\033[m')
+print(f'7  * {num} = \033[34m{num * 7}\033[m')
+print(f'8  * {num} = \033[34m{num * 8}\033[m')
+print(f'9  * {num} = \033[34m{num * 9}\033[m')
+print(f'10 * {num} = \033[34m{num * 10}\033[m')
+print('\033[33m=*=\033[m'*5)
