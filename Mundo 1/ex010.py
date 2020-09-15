fc7d6621969964c6269e24e9a60a3f7e2a3111c6
Judite Denise <judigunkel@gmail.com>
2020-09-15 16:08:28 -0300

    ex010

---
+++ Mundo 1/ex010.py
@@ -0,0 +1,7 @@
+"""
+Crie um programa que leia quanto dinehiro uma pessoa tem na carteira e mostre
+quantos dólares ela pode comprar
+"""
+
+n = float(input('Quanto dinheiro você tem na carteira? R$ '))
+print(f'Com \033[31mR$ {n:.2f}\033[m você pode comprar \033[36mUS$ {n/4.11:.2f}\033[m')
