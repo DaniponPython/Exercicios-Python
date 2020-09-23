36d8b354c2ccf0d172068c09c03f98c38e92d640
Judite Denise <71028921+judigunkel@users.noreply.github.com>
2020-09-22 21:11:36 -0300

    Update ex020.py

--- Mundo 1/ex020.py
+++ Mundo 1/ex020.py
@@ -5,10 +5,10 @@ mostre a ordem sorteada.
 """
 from random import shuffle
 
-al1 = input('Primeiro aluno: ')
-al2 = input('Segundo aluno: ')
-al3 = input('Terceiro aluno: ')
-al4 = input('Quarto aluno: ')
+al1 = input('Primeiro aluno: ').title()
+al2 = input('Segundo aluno: ').title()
+al3 = input('Terceiro aluno: ').title()
+al4 = input('Quarto aluno: ').title()
 lista = [al1, al2, al3, al4]
 shuffle(lista)
 print(f'A ordem de apresentação seŕa: \n\033[1;30;41m{lista}\033[m')
