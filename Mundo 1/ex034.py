152e731b563282769955f6f71b1d0325603da52c
Judite Denise <judigunkel@gmail.com>
2020-09-29 10:16:46 -0300

    ex034

---
+++ Mundo 1/ex034.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que pergunte o salário de um funcionário e calcule o valor do
+seu aumento. Para salários superiores a R$ 1250,00 calcule um aumento de 10%.
+Para os inferiores ou iguais, o aumento é igual a 15%
+"""
+salario = float(input('Qual é o salário do funcionário? R$'))
+if salario > 1250:
+    n_salario = salario * 1.1
+else:
+    n_salario = salario * 1.15
+print(f'Quem ganhava R$ {salario:.2f} passa a ganhar R$ {n_salario:.2f} agora')
