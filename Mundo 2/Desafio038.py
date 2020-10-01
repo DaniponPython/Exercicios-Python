c94400b800d4c759da13028c635095564458c8d8
Judite Denise <judigunkel@gmail.com>
2020-09-30 21:15:49 -0300

    ex038

---
+++ Mundo 2/Desafio038.py
@@ -0,0 +1,15 @@
+"""
+Escreva um programa que  leia dois números inteiros e compare-os, mostrando na
+tela uma mensagem:
+O primeiro valor é maior
+O segundo valor é maior
+Não existe valor maior, os dois são iguais
+"""
+num1 = float(input('Primeiro número: '))
+num2 = float(input('Segundo número: '))
+if num1 > num2:
+    print('O PRIMEIRO valor é maior')
+elif num2 > num1:
+    print('O SEGUNDO número é maior')
+else:
+    print('Os dois números são IGUAIS')
